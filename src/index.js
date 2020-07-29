056071a0cc05d485fd65971dca8edfc57be0bff2
Emerson Bezerra <emersondasb@outlook.com>
2020-07-29 18:11:44 -0300

    Implementação das páginas de cadastro e carrossel

--- src/index.js
+++ src/index.js
@@ -1,11 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home/Home';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import CadastroVideo from './pages/cadastro/Video/';
+import CadastroCategoria from './pages/cadastro/Categoria';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+
+      <Route exact path="/" component={Home} />
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
+      <Route component={()=> (<div>Página não existente (Erro 404)</div>)} />
+
+
+    </Switch>
+
+  </BrowserRouter>,
   document.getElementById('root')
 );
