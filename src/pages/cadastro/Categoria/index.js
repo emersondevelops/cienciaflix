420ef959d7175e107b8808ab12846682e887ebdd
Emerson Bezerra <emersondasb@outlook.com>
2020-08-04 14:14:43 -0300

    Alteração do endereço de request do localhost para servidor

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -4,7 +4,6 @@ import PageDefault from '../../../components/PageDefault';
 import FormField from '../../../components/FormField';
 import Button from '../../../components/Button';
 import useForm from '../../../hooks/useForm';
-import config from '../../../config';
 
 function CadastroCategoria() {
   const valoresIniciais = {
@@ -13,13 +12,13 @@ function CadastroCategoria() {
     cor: '',
   };
 
-  const URL_CATEGORIES = `${config.URL_BACKEND_TOP}/categorias`;
   const { handleChange, values, clearForm } = useForm(valoresIniciais);
   const [categorias, setCategorias] = useState([]);
 
   useEffect(() => {
+    const URL = 'https://cienciasflix.herokuapp.com/categorias';
     if (window.location.href.includes('localhost')) {
-      fetch(URL_CATEGORIES)
+      fetch(URL)
         .then(async (respostaDoServer) => {
           if (respostaDoServer.ok) {
             const resposta = await respostaDoServer.json();
